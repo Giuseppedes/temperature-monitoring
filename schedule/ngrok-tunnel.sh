ba437411def845365b75491f801d76596f89f3c9
giuseppe <giuseppedes@hotmail.it>
2020-03-14 19:56:51 +0100

    add IP address to email

--- schedule/ngrok-tunnel.sh
+++ schedule/ngrok-tunnel.sh
@@ -2,11 +2,20 @@
 
 cd /home/pi
 
-printf "Subject: temperature monitoring link\n\n" > ngrok-output.txt 
+printf "Subject: temperature monitoring link\n\n" > ngrok-output.txt
+
+printf "IP\n" >> ngrok-output.txt
+
+ip addr show wlan0 >> ngrok-output.txt
+
+printf "\nPublic IP\n" >> ngrok-output.txt
+
+curl ifconfig.me >> ngrok-output.txt
+
+printf "\n\nNGROK OUTPUT\n" >> ngrok-output.txt
 
 ./ngrok http 3000 -log=stdout >> ngrok-output.txt &
 
-sleep 10
+sleep 30
 
 cat ngrok-output.txt | msmtp giuseppedes90@gmail.com
-
